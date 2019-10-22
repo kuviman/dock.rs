ad0fe06171a5d55d83cd3540d229b3f2a03dea7f
kuviman <kuviman@gmail.com>
2019-10-22 12:39:53 +0400

    Specify commit of osxcross

--- install-osxcross.sh
+++ install-osxcross.sh
@@ -5,6 +5,7 @@ set -e
 cd /tmp
 git clone https://github.com/tpoechtrager/osxcross
 cd osxcross
+git checkout d3392f4
 wget -q https://s3.dockerproject.org/darwin/v2/MacOSX10.11.sdk.tar.xz --directory-prefix=tarballs
 sed -i -e 's|-march=native||g' build_clang.sh wrapper/build.sh
 UNATTENDED=yes OSX_VERSION_MIN=10.7 ./build.sh
