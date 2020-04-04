796d01a3713ac6c16fe53c3a8d38abd2f242588a
kuviman <kuviman@gmail.com>
2020-04-04 19:22:39 +0400

    Fix osxcross install script

--- install-osxcross.sh
+++ install-osxcross.sh
@@ -6,5 +6,4 @@ cd /tmp
 git clone https://github.com/tpoechtrager/osxcross
 cd osxcross
 wget -q https://s3.dockerproject.org/darwin/v2/MacOSX10.11.sdk.tar.xz --directory-prefix=tarballs
-sed -i -e 's|-march=native||g' build_clang.sh wrapper/build.sh
-UNATTENDED=yes OSX_VERSION_MIN=10.7 ./build.sh
+PORTABLE=yes UNATTENDED=yes OSX_VERSION_MIN=10.7 ./build.sh
