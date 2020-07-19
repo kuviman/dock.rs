0b315e6d4b7e7bd8646aa18e405697a305516f61
kuviman <kuviman@gmail.com>
2020-07-20 03:39:19 +0400

    Downgrade to MacOS 1.13

--- macos/install-osxcross.sh
+++ macos/install-osxcross.sh
@@ -5,5 +5,5 @@ set -e
 cd /tmp
 git clone https://github.com/tpoechtrager/osxcross
 cd osxcross
-wget -q https://github.com/phracker/MacOSX-SDKs/releases/download/10.15/MacOSX10.15.sdk.tar.xz --directory-prefix=tarballs
+wget -q https://github.com/phracker/MacOSX-SDKs/releases/download/10.15/MacOSX10.13.sdk.tar.xz --directory-prefix=tarballs
 PORTABLE=yes UNATTENDED=yes OSX_VERSION_MIN=10.7 ./build.sh
